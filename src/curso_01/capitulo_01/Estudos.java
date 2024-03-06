2885c9050c41b086f0fb8e4673303bdc905a7ea8
jeffersonrolino <jefferson.rolino@gmail.com>
2024-03-06 07:46:15 -0300

    Desafios - Curso 01 - Capítulo 01

---
+++ src/curso_01/capitulo_01/Estudos.java
@@ -0,0 +1,8 @@
+package curso_01.capitulo_01;
+
+public class Estudos {
+    public static void main(String[] args) {
+        System.out.println("2 + 2 é igual a 4.");
+        System.out.println("O quadrado da hipotenusa é igual à soma dos quadrados dos catetos.");
+    }
+}
---
+++ src/curso_01/capitulo_01/Perfil.java
@@ -0,0 +1,8 @@
+package curso_01.capitulo_01;
+
+public class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Olá Jefferson.");
+        System.out.println("Tudo bem?");
+    }
+}
